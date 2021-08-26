ae5f5f3bf7c21830a2c027f9dcfc9124bc302a75
Tom Austin <tom@tomaustin.xyz>
2021-08-26 16:07:39 +0100

    Added second storage for scripts

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -43,6 +43,20 @@ resource "azurerm_storage_account" "functionstorage" {
   account_replication_type = "LRS"
 }
 
+resource "azurerm_storage_account" "scriptstorage" {
+  name                     = "peepscriptstorage"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "scriptcontainer" {
+  name                  = "scripts"
+  storage_account_name  = azurerm_storage_account.scriptstorage.name
+  container_access_type = "private"
+}
+
 resource "azurerm_app_service_plan" "serviceplan" {
   name                = "peepquotesp"
   resource_group_name = azurerm_resource_group.rg.name
