1b0b679bf7ab22fcf762e80315f1d5af2e396456
Tom Austin <tom@tomaustin.xyz>
2021-08-26 16:09:05 +0100

    New name for storage

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -44,7 +44,7 @@ resource "azurerm_storage_account" "functionstorage" {
 }
 
 resource "azurerm_storage_account" "scriptstorage" {
-  name                     = "peepscriptstorage"
+  name                     = "peepqscriptstorage"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
