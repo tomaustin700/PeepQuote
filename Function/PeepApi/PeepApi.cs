851805d78172d71e69c8083e84b806b00cf57c8d
Tom Austin <tom@tomaustin.xyz>
2021-08-27 15:46:00 +0100

    Added quote search

--- Function/PeepApi/PeepApi.cs
+++ Function/PeepApi/PeepApi.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
@@ -58,6 +59,60 @@ namespace PeepApi
             var response = req.CreateResponse(HttpStatusCode.OK);
 
             await response.WriteAsJsonAsync(new { quote = quotes[indexValue].Item1, episode = quotes[indexValue].Item2 });
+
+            return response;
+        }
+
+        [Function(nameof(QuoteSearch))]
+        public async Task<HttpResponseData> QuoteSearch([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req,
+            FunctionContext executionContext)
+        {
+            var search = executionContext.BindingContext.BindingData["search"].ToString();
+
+            var searchCleaned = new string(search.Where(c => !char.IsPunctuation(c)).ToArray());
+
+            var quotes = new List<(string, string)>();
+            var matches = new List<(string, string)>();
+            foreach (BlobItem blob in _blobContainerClient.GetBlobs())
+            {
+                BlobClient blobClient = _blobContainerClient.GetBlobClient(blob.Name);
+
+                var content = await blobClient.DownloadAsync();
+                var text = content.Value.Content;
+                string episodeName = null;
+
+                using (var streamReader = new StreamReader(text))
+                {
+                    while (!streamReader.EndOfStream)
+                    {
+                        var line = await streamReader.ReadLineAsync();
+
+                        if (string.IsNullOrEmpty(episodeName))
+                            episodeName = line;
+                        else
+                        {
+                            if (!line.StartsWith("["))
+                                quotes.Add((line, blob.Name.Replace(".txt", "") + $" - {episodeName}"));
+                        }
+                    }
+                }
+            }
+
+            foreach (var quote in quotes)
+            {
+                var quoteCleaned = new string(quote.Item1.Where(c => !char.IsPunctuation(c)).ToArray());
+
+                if (Regex.IsMatch(quoteCleaned.ToLower(), @$"\b{searchCleaned.ToLower()}\b"))
+                {
+                    matches.Add(quote);
+                }
+            }
+
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+
+            await response.WriteAsJsonAsync(matches.Select(a => new { quote = a.Item1, episode = a.Item2 }));
+
 
             return response;
         }
