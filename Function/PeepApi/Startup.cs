b058e5f2f32a05df531a75e74851571347aa0ab2
Tom Austin <tom@tomaustin.xyz>
2022-04-02 16:42:33 +0100

    fix conn string

--- Function/PeepApi/Startup.cs
+++ Function/PeepApi/Startup.cs
@@ -13,8 +13,7 @@ namespace PeepApi
     {
         public override void Configure(IFunctionsHostBuilder builder)
         {
-            //builder.Services.AddAzureClients(azure => azure.AddBlobServiceClient(Environment.GetEnvironmentVariable("ConnectionString")));
-            builder.Services.AddAzureClients(azure => azure.AddBlobServiceClient("DefaultEndpointsProtocol=https;AccountName=peepqscriptstorage;AccountKey=+Yt1Vsyi0CWQEOREYivTnP5v8bVhfVhywcs8wyRYSzFGYZ4LAzwsvNKS84NjGIDsUuBqRo/L4mTmdipY1zSOkw==;EndpointSuffix=core.windows.net"));
+            builder.Services.AddAzureClients(azure => azure.AddBlobServiceClient(Environment.GetEnvironmentVariable("ConnectionString")));
         }
     }
 }
