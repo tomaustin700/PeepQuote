52df5999cb30b2f9fd26099f6209c0038d4e41b0
Tom Austin <tom@tomaustin.xyz>
2022-07-31 14:37:31 +0100

    reset connection string

--- Function/PeepApi/Startup.cs
+++ Function/PeepApi/Startup.cs
@@ -13,7 +13,7 @@ namespace PeepApi
     {
         public override void Configure(IFunctionsHostBuilder builder)
         {
-            builder.Services.AddAzureClients(azure => azure.AddBlobServiceClient("DefaultEndpointsProtocol=https;AccountName=peepqscriptstorage;AccountKey=hyjKwOlgq8VoeTcB74ZI0CHignCOPpUvdWbiZGaSapJlI4x2agy0K/cbzhTSRrI8mS5RJ2uvjquwxIgi6rQAGQ==;EndpointSuffix=core.windows.net"));
+            builder.Services.AddAzureClients(azure => azure.AddBlobServiceClient(Environment.GetEnvironmentVariable("ConnectionString")));
         }
     }
 }
