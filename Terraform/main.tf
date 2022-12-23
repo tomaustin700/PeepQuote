f6de0795c74eb5d4c8c05a1e4d2ef9ccfeb4246d
Tom Austin <Tom.Austin@streets-heaver.com>
2022-12-23 10:28:19 +0000

    added tf conf to create new storage account

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -92,3 +92,19 @@ resource "azurerm_function_app" "function" {
 
 }
 
+resource "azurerm_storage_account" "imagestorage" {
+  name                     = "peepimagestorage"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  static_website {
+    
+  }
+}
+
+resource "azurerm_storage_container" "imagecontainer" {
+  name                  = "imagecontainer"
+  storage_account_name  = azurerm_storage_account.imagestorage.name
+  container_access_type = "private"
+}
